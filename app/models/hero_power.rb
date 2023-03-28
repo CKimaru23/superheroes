82e7139702f76281276ab23661fb07dd72d06f96
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-03-28 08:51:44 +0300

    validation

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -3,5 +3,5 @@ class HeroPower < ApplicationRecord
     belongs_to :power
 
     validates :strength, inclusion: { in: ['Strong', 'Weak', 'Average'] }
-    validates :power_id, uniqueness: { scope: :hero_id, message: "already added to the restaurant" }
+    validates :power_id, uniqueness: { scope: :hero_id, message: "already added." }
 end
